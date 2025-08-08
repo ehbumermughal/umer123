994599ed1f281a5c91b6f784b14e6c82d80ff33e
umer mughal <ehbumermughal@gmaill.com>
2025-08-08 10:11:17 +0500

    first commit

---
+++ python/index.py
@@ -0,0 +1,42 @@
+# print("hellow-python")
+# ab = 3
+
+# string = "this is string"
+# float = 4.0
+# char = 'h'
+
+# double = 5454548215.544
+# print(type(ab),ab)
+# print(type(string))
+
+# print(type(float))
+
+# print(type(char))
+# mamu=110
+# if mamu > 10:
+#     print(True)
+# else:
+#     print(False)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+# my_list1 =["umer" ,"ahmed" ,"usamn", "fakhir"]
+# my_tupple = ("ahmed" ,"usamn", "fakhir")
+
+# my_list1.append('fakhir')
+# print(my_list1)
+# print(my_tupple) 
+# print(my_list1[:3])
+
+# list1= [100,200,50,30,90]
+# list2= [10,15,1000,65,160]
+# print(list1.sort())
+
+# join_list =list1 +list2
+# print(list1)
+# print(list2)
+# print(join_list)
+# second_list =[50,60,70,80,100]
+# print(second_list[4])
+# second_list.sort()
---
+++ python/string.py
@@ -0,0 +1,62 @@
+#here we will learn about string
+
+
+
+a = "this is string"
+
+#print(a[:6])  #slicing from start 
+#print(a[6:])  #slicing from end
+#print(a[-4:-2])
+#print(a[-4:2])
+#print(a.upper())
+#print(a.split())
+#print(a.strip())
+#print(a.split("^"))
+
+
+#a =  "full stack"
+#b = "courses"
+
+
+#print(a+b)
+
+#a = "hellow"
+# b = "world"
+# c = a+""
+#print(c)
+# name = "mamu" 
+# namw22 = "kjsdhjkfhs"
+# greeting =f"Hey {name},hellow how are you "
+
+# print(greeting)
+#operator
+#equal to operator
+ 
+# a = 10
+# b = a
+
+# print(b)
+# print(type(a))
+# print(str(b))
+
+#compare operator
+# a = 12
+# b = 13
+# print (a==b)
+# print(a>=b)
+# print(a<=b)
+# print(a>b)
+# print(a<b)
+
+#arthmatic operator
+# a =10
+# b=15
+# add = a + b
+# print(add)
+
+a = 5 
+b = 7
+c = 5
+d = 5
+  
+print(a==c or a ==b)
