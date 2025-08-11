6420c7e6de404985285019597373d178cc84876e
umer mughal <ehbumermughal@gmaill.com>
2025-08-11 09:08:13 +0500

    read polan loops+big object

---
+++ python/dict.py
@@ -0,0 +1,15 @@
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# current_grate = {2022:'81%',2023:'90%', 2024:'101'} 
+my_dict = {
+    "banana": 3,
+    "apple": 5,
+    "cherry": 2
+}
+
+# Sort by keys
+sorted_by_keys = dict(sorted(my_dict.items()))
+print("Sorted by keys:", sorted_by_keys)
---
+++ python/loop.py
@@ -0,0 +1,32 @@
+# fruits = ["apple", "banana", "cherry"]
+# fruits(2)
+
+# print(fruits(2))
+# for abc in fruits:
+#     print(abc) 
+
+for ali in range( 1 ,10): 
+    print(ali)
+
+if ali ==5:
+    print ("number is 5")        
+else :
+    print("ali is here")
+cd
+
+
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+#   if x == "banana":
+#     break
+  
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+
+# for x in adj:
+#   for y in fruits:
+#     print(x, y)
+  
