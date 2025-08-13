bad7f0d5608212a5eef17ebcd6b01ec0d3e500e9
umer mughal <ehbumermughal@gmaill.com>
2025-08-13 10:27:41 +0500

    read :function.py

---
+++ python/functions.py
@@ -0,0 +1,55 @@
+
+
+
+# def add_func (a,b):
+#     a =5
+#     b= 10
+#     c =a+b
+#     print('return calue')
+#     return c
+# a = 10
+# b = 25
+# result = add_func(a,b)
+
+# print (result)
+# def add_func(a,b):
+#     c =a+b
+#     print("return value")
+#     return c
+    
+# result = add_func(a,b)
+# print(result)
+
+
+# def my_function(**args):
+#   print("His last name is " + args["lname"])
+
+
+# my_function(fname = "caliber", lname = "usman")  
+
+#list loop 
+# def get_list ():
+numbers = [ 1,2,"nothing",4,5,"i am here",7,8,9]
+
+# number =get_list()
+
+for number in numbers:
+    print(number)
+    if number == 5:
+        print(5)
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
